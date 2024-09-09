4e0c5b886b3945f51f6572ebc75c9dfca880d50c
UnzilaShakir <unzilashakir21@gmail.com>
2024-09-10 03:56:01 +0500

    add files and folders

---
+++ app.js
@@ -0,0 +1,112 @@
+// Chapter1
+// Qno1
+alert('Mr Jhony')
+
+// Qno 2
+alert('please enter a valid password')
+
+// Qno 3
+alert('Welcome to js land\n Happy coding')
+
+// Qno 4
+alert('Welcome to js land')
+alert('Happy Coding\n Prevent this page from creating additional dialogs')
+
+// Qno 5
+alert('Hello I can run Js through my browsers developer console')
+console.log('Hello I can run Js through my browsers developer console')
+
+// Chapter 2
+// Qno 1
+var username
+
+// Qno 2
+var myname = 'unzila shakir'
+
+// Qno 3
+var message = "Hello World"
+alert(message)
+
+// Qno 4
+var name = 'unzila'
+alert(name)
+var age = '19 years old'
+alert(age)
+var certificate = 'Certified Mobile Application developement'
+alert(certificate)
+
+// Qno 5
+var abc = 'PIZZA \nPIZZ \nPIZ \nPI \nP'
+alert(abc)
+
+// Qno 6
+var email = 'My email address is unzilashakir21@gmail.com'
+alert(email)
+
+
+// Qno 7
+var book = "I am trying to learn from the book A smarter way to learn Javascript"
+alert(book)
+
+// Qno 8
+document.write('yah! i can write HTML content through javaScript <br>')
+
+// Qno 9
+var xyz = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(xyz)
+
+// Chapter 3
+// Qno 1
+var age = 'I am 19 years old'
+alert(age)
+
+// Qno 2
+var n = 'You have visited this site 14 times'
+alert(n)
+
+// Qno 3
+var birthyear = 'My birthyear is 2004'
+document.write(birthyear +'<br>')
+
+// Qno 4
+var visitor = 'Mr john Doe'
+var product = "t-shirts"
+var Order = "5 t-shirts"
+
+document.write('John doe ordered 5 T-shirt on xyz clothing store <br>')
+
+// Chapter 4
+// Qno 1
+var myname, firstname, lastname = 'Mr Don'
+
+// Qno 2
+// legal variable
+var abc = 'my first class';
+var $ = '.';
+var _ = '.';
+var string = '.';
+var xyz = '.';
+
+// illegal variables
+// var 123 = '.'
+// var 2myname ='.'
+// var ! = '.'
+// var % = '.'
+// var * = '.'
+
+// Qno 3
+document.write ('Rules for naming JS variables' + '<br>' )
+document.write(' \nVariable names can only contain ,numbers,$and _. For example $my _1st variable' + '<br>')
+document.write(' \nVariable names can only contain ,numbers,$and _. For example $my _1st variable' + '<br>')
+document.write('variable must be begin with a letter,$or _for example $name , _name or name' + '<br>')
+document.write('variables name are case sensitive' + '<br>')
+
+
+
+
+
+
+
+
+
+
